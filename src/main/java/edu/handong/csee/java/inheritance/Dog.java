c2756050b663143401a63b4f31e9ffecb4fd4742
hyunwoo <hyunwoo@172.17.138.233>
2019-05-01 15:12:56 +0900

    Lab14

---
+++ src/main/java/edu/handong/csee/java/inheritance/Dog.java
@@ -0,0 +1,8 @@
+package edu.handong.csee.java.inheritance;
+
+public class Dog extends Animal {
+	public void testInstanceMethod() {
+        System.out.println("The instance method in Dog");
+    }
+
+}
